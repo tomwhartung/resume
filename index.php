ae280c9d9152cfe24813cec8f6f45b40276764a6
Tom W Hartung <tomwhartung@gmail.com>
2016-04-05 10:17:53 -0600

    Updated range of years on copyright at bottom of the page.

--- index.php
+++ index.php
@@ -395,7 +395,7 @@ print '<link rel="stylesheet" type="text/css" ' .
 			<li class="footer_links"><a href="http://tomhartung.com">tomhartung.com</a></li>
 		</ul>
 		<hr />
-		<p class="footer">&copy; 2001-2015 Tom W. Hartung, P. O. Box 18216, Denver, CO 80218, 303-863-1757</p>
+		<p class="footer">&copy; 2001-2016 Tom W. Hartung, P. O. Box 18216, Denver, CO 80218, 303-863-1757</p>
 		<hr />
 	</footer>
 <?php  else: ?>
